7e8e486de6fff0b9553c43c3f2c26bbda4e50f12
Marco <marcogara24@gmail.com>
2025-07-04 13:45:17 +0200

    updated folder

---
+++ src/main/java/com/example/demo_springboot_store/PaymentService.java
@@ -0,0 +1,5 @@
+package com.example.demo_springboot_store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
--- store/src/main/java/com/example/demo_springboot_store/PaymentService.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo_springboot_store;
-
-public interface PaymentService {
-    void processPayment(double amount);
-}
