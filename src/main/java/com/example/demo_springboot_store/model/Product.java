d8fdcfd8ddcf812a2e6d4d9320b527dae0d92c04
Marco <marcogara24@gmail.com>
2025-07-05 16:11:21 +0200

    added setters to pruduct POJO

--- src/main/java/com/example/demo_springboot_store/model/Product.java
+++ src/main/java/com/example/demo_springboot_store/model/Product.java
@@ -24,4 +24,16 @@ public class Product {
     public String getDescription() {
         return description;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
