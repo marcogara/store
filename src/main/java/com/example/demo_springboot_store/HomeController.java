52b2f7f009586af982d2877c6711b8b36c18e7a5
Marco <marcogara24@gmail.com>
2025-07-07 11:01:34 +0200

    added logout

--- src/main/java/com/example/demo_springboot_store/HomeController.java
+++ src/main/java/com/example/demo_springboot_store/HomeController.java
@@ -1,12 +1,14 @@
 package com.example.demo_springboot_store;
 
+import com.example.demo_springboot_store.model.User;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -16,13 +18,13 @@ public class HomeController {
     private String appName;
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("user", new User());
         return "index";
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String username,
-                        @RequestParam String password,
+    public String login(@ModelAttribute User user,
                         HttpSession session,
                         Model model) {
 
@@ -32,8 +34,9 @@ public class HomeController {
                 "user", "userpass"
         );
 
-        if (users.containsKey(username) && users.get(username).equals(password)) {
-            session.setAttribute("user", username); // Store user in session
+        String password = users.get(user.getUsername());
+        if (password != null && password.equals(user.getPassword()))  {
+            session.setAttribute("user", user.getUsername()); // Store user in session
             return "redirect:/dashboard"; // Redirect after successful login
         }
 
@@ -41,4 +44,20 @@ public class HomeController {
         return "index"; // Stay on login page if failed
     }
 
+    @GetMapping("/dashboard")
+    public String dashboard(HttpSession session, Model model) {
+        Object loggedUser = session.getAttribute("user");
+
+        if (loggedUser == null) {
+            return "redirect:/";
+        }
+        model.addAttribute("username", loggedUser);
+        return "dashboard";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate(); // Destroys the session, logs the user out
+        return "redirect:/";  // Redirect back to login page
+    }
 }
